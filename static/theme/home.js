bf8f58a8343f0fa99dc6c584e7cf583b43ead4de
“jonyshi” <jonyshi@126.com>
2019-04-15 16:36:59 +0800

    css 前缀问题修改

--- static/theme/home.js
+++ static/theme/home.js
@@ -1,11 +1,1 @@
-webpackJsonp([2],[
-/* 0 */,
-/* 1 */
-/***/ (function(module, exports, __webpack_require__) {
-
-__webpack_require__("j1ja");
-module.exports = __webpack_require__("uI4+");
-
-
-/***/ })
-],[1]);
+webpackJsonp([2],[,function(n,o,p){p("j1ja"),n.exports=p("uI4+")}],[1]);
