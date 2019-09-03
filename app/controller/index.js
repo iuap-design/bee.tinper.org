948959e360611fd6046312580c85dd4de0b7ade7
liushaozhen <804199994@qq.com>
2019-09-03 15:10:02 +0800

    issue地址修改

--- app/controller/index.js
+++ app/controller/index.js
@@ -141,7 +141,7 @@ module.exports = {
         component +
         "/edit/master/docs/api.md' class='pencil'  target='_blank' title='在github上编辑此页'><i class='uf uf-pencil-s' style='font-size: 20px;padding-left: 10px;'></i></a>" +
         "<div class='title-right'>" +
-        "<a class='title-tag' href='https://github.com/tinper-bee/" + component + "/issues/new' target='_blank'><i class='uf uf-qm-c'></i><span>issue</span></a>" +
+        "<a class='title-tag' href='https://github.com/iuap-design/tinper-bee/issues/new' target='_blank'><i class='uf uf-qm-c'></i><span>issue</span></a>" +
         listStr +
         "</div>"
       );
