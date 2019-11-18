75919109b02fa9af90b1d64ad7fc2fa48814a599
liushaozhen <804199994@qq.com>
2019-11-18 10:42:33 +0800

    getComponentTwo修改

--- app/utils/getComponentTwo.js
+++ app/utils/getComponentTwo.js
@@ -2,19 +2,17 @@ const fs = require('fs-extra');
 const download = require('download-git-repo')
 let componentsSource = require('../../static/componentsSource.json');
 let length = Object.keys(componentsSource).length;
-const oldLength = Object.keys(componentsSource).length;
+let components = require('../../static/componentsSource.json');
 
 
 function consoleItem(length){
     if(length<=3){
-        for(let i = 0;i<length;i++){
-            let it = oldLength - length - i -1;
-            console.log(`剩下：${Object.keys(componentsSource)[it]}`)
-        }
+        console.log(Object.keys(components))
     }
 }
 
 if(fs.pathExistsSync(`tinper-acs/${item}`)){
+    delete components[item]
     console.log(`😀 ${item} 已存在,还剩  ${--length}  个`);
     consoleItem(length);   
 }else{
@@ -25,6 +23,7 @@ if(fs.pathExistsSync(`tinper-acs/${item}`)){
             if (error) {
                 console.log(`❌ download ${item} 失败,还剩  ${length}  个` + error);
             }else{
+                delete components[item]
                 console.log(`😀 download ${item} 成功,还剩  ${--length}  个`);
                 consoleItem(length)
             }
